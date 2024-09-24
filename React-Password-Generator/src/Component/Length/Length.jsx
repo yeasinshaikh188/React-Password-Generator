69a7ab6dec42206e3d92735fcd8d74d183848ee1
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-09-24 22:02:03 +0600

    creat a file for password generator

---
+++ React-Password-Generator/src/Component/Length/Length.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Length = () => {
+
+    const [length, setLength] = useState(8);
+    const [numberAllowed, setNumberAllowed] = useState(false); 
+    const [charAllowed, setCharAllowed] = useState(false); 
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Length;
